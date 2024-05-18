3ab4c717bb698125a2b05c8bf47095853aa8b567
Agamane2343 <168989245+Agamane2343@users.noreply.github.com>
2024-05-18 17:21:46 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,30 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+count = 0
+count = int(count)
+
+students = sorted(students)
+#print (students)
+#print (len(students))
+#print (len(grades))
+
+grades [count] = sum(grades[count]) / len(grades[count])
+count += 1
+grades [count] = sum(grades[count]) / len(grades[count])
+count += 1
+grades [count] = sum(grades[count]) / len(grades[count])
+count += 1
+grades [count] = sum(grades[count]) / len(grades[count])
+count += 1
+grades [count] = sum(grades[count]) / len(grades[count])
+count += 1
+#grades [count] = sum(grades[count]) / len(grades[count])
+#count += 1
+
+
+#print (grades)
+
+k = dict(zip(students[0:5], grades[0:5]))
+print (k)
+
+
