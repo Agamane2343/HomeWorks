69b3cab7d9ba67e6515517949798ea9fa8df6f9d
Agamane2343 <168989245+Agamane2343@users.noreply.github.com>
2024-05-22 15:20:30 +0500

    Add files via upload

---
+++ Функции в Python.Функция с параметром.py
@@ -0,0 +1,9 @@
+def print_params(a = input("Введите первое значение "),
+                 b = input("Введите второе значение ")):
+    print ((a, b) * 2)
+
+
+print_params('банан', ('ворона'))
+print_params()
+print_params()
+print_params()
