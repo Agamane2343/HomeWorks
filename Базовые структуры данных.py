a6ba72f7ce7ba947f390e8193f22b30f0995aa5b
Agamane2343 <168989245+Agamane2343@users.noreply.github.com>
2025-01-22 18:06:41 +0500

    Add files via upload

---
+++ Базовые структуры данных.py
@@ -0,0 +1,25 @@
+# 1st program
+
+print  (9 ** 0.5 * 5)
+
+# 2nd program
+
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print ((2 * (2 + 2)) == (2 * 2 + 2))
+
+#4th program
+
+print (int((float('123.456') * 10 // 1 % 10)))
+
+#3rd program old
+
+print ((1234 // 10 % 100) + (5678 // 10 % 100))
+
+#4th program old
+
+print ((13.42 // 1 == 42.13 % 1 * 100 // 1) or (13.42 % 1 * 100 // 1 == 42.13 // 1))
